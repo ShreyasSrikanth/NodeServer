53f0458acd73568ce730f73d3339b06b87fb8325
Shreyas Srikanth <sshreyas567@gmail.com>
2023-11-14 20:46:03 +0530

    changes related to debugger

--- routes.js
+++ routes.js
@@ -18,7 +18,6 @@ const requestHandler = (req,res) =>{
         res.write('</html>');
         return res.end();
        }
-      
     if (url === '/message' && method === 'POST'){
         const body = [];
         req.on('data',(chunk) =>{
